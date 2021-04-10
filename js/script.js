136f3bf139cf7c59fb9eb35d1ac8f78ba3ab0247
Dev-MahmoudSayed <ms5566913@gmail.com>
2021-04-10 22:37:19 +0200

    first

---
+++ js/script.js
@@ -0,0 +1,83 @@
+// function change(color){
+//     document.body.style.background = color;
+
+// }
+// var person =['mohamed',20,'giza', 'single']
+// for(var x=0; x < person.length; x++){
+//   var res = '<li>' +person[x] +'</li>';
+//   document.getElementById('list').innerHTML +=res;
+// }
+// var persons = [];
+
+// function add(){
+//   var name = document.getElementById('name').Value;
+//   var age = document.getElementById('age').Value;
+//   var person = {"name": name,"age":age};
+//   persons.push(person);
+//   console.log(persons);
+// }
+// for( var x = 0; x < 5; x++){
+//   document.getElementById("res").innerHTML += "*" + "<br>";
+
+// }
+ 
+// for( let y = 0; y<10; y++){
+ 
+//   console.log(y);
+
+// }
+// let name = "mahmoud";
+// let age = 21;
+// console.log(`my name is ${name} and my age ${age}`);
+
+
+// function add(){
+//   // alert ('mohmed');
+//   // let name = document.getElementById("name").value;
+//   let name = $('#name').val();
+//   // alert (name);
+//   if (name =='') {
+//   //  document.getElementById("name").classList.add("error");
+//      $("#name").addClass("error");
+//   } else {
+//     // document.getElementById("name").classList.remove("error");
+//     $("#name").removeClass("error");
+//     alert(name);
+//   }
+
+// }
+
+// $("#click").click(function(){
+//   $("#div").show(function(){
+//     $("#div").slideUp();
+//   });
+// });
+
+
+// $("#name").keypress(function(){
+//   let name = $("#name").val();
+//  alert(name);
+
+// })
+// function add(){
+//   let name = $("#name").val();
+//   $("#ss").text(name)
+// }
+// function plus(){
+//   let html = `
+//   <input class="form-control" type="text" id="name">
+  
+//   `;
+
+//   $("#res").append(html);
+// }
+// $("#change").click(function (){
+//   $(this).toggleClass("itemc");
+// });
+// $(".navcustom ul li").click(function(){
+//   $(this).children().toggleClass("active");
+//   $(this).siblings().children().removeClass("active");
+// });
+// $(document).ready (function () {
+//   $(".loader").delay(2000) .fadeOut('slow');
+// });
